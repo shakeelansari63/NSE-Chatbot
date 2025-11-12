cc429539e09805b3af7cb961f2caf1979796b93f
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-11-12 18:17:53 +0530

    run on server port 8000

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
+
 from chatui import ui
 
 load_dotenv(override=True)
 
 
 def main():
-    ui.launch()
+    ui.launch(server_port=8000, server_name="0.0.0.0")
 
 
 if __name__ == "__main__":
